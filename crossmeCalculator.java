72c78df3dfb0f5a03f14e66d23e0b246255544e1
afkCYa <chenya17@hotmail.com>
2024-05-31 23:08:03 -0400

    added file
    crossme calculator

---
+++ crossmeCalculator.java
@@ -0,0 +1,11 @@
+package crossMetest;
+
+public class crossmeCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bye world");
+		
+	}
+
+}
